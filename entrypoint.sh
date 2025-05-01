1e9c6206399d13d0c9f88dcb8cc4cf118c2f707c
Aymenibr <aynibrahim01@gmail.com>
2025-05-02 00:32:59 +0300

    fixing the static page

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Run database migrations
 conda run -n blogenv python manage.py migrate
-
-# Start Django server
+conda run -n blogenv python manage.py collectstatic --noinput
 conda run -n blogenv python manage.py runserver 0.0.0.0:8000
